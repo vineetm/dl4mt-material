7e5d434c7e49a2d246b4445423747c5412a0e864
Vineet Kumar <vineeku6@in.ibm.com>
2016-01-19 13:28:24 +0530

    Helper tool to compute test error on a trained model

---
+++ session2/get_error.py
@@ -0,0 +1,64 @@
+import logging, theano, argparse, os
+import cPickle as pkl
+from nmt import build_model, pred_probs, prepare_data, init_params, load_params, init_tparams
+from data_iterator import TextIterator
+
+
+def get_error(model, test_src, test_target):
+    profile=False
+
+    # reload options
+    f = open('%s.pkl' % model, 'rb')
+    model_options = pkl.load(f)
+    logging.info(model_options)
+
+    logging.info('Building model')
+    params = init_params(model_options)
+
+    # reload parameters
+    params = load_params(model, params)
+    tparams = init_tparams(params)
+
+    trng, use_noise, \
+        x, x_mask, y, y_mask, \
+        opt_ret, \
+        cost = \
+        build_model(tparams, model_options)
+    inps = [x, x_mask, y, y_mask]
+z
+    dict_src = os.path.join(model_options['baseDir'], model_options['dictionaries'][0])
+    dict_target = os.path.join(model_options['baseDir'], model_options['dictionaries'][1])
+
+    valid = TextIterator(test_src, test_target,
+                         dict_src,
+                         dict_target,
+                         n_words_source=model_options['n_words_src'],
+                         n_words_target=model_options['n_words'],
+                         batch_size=model_options['valid_batch_size'],
+                         maxlen=model_options['maxlen'])
+
+    logging.info('Building f_log_probs...')
+    f_log_probs = theano.function(inps, cost, profile=profile)
+    valid_errs = pred_probs(f_log_probs, prepare_data,
+                                        model_options, valid)
+    valid_err = valid_errs.mean()
+    logging.info('Valid Error:%s'% (str(valid_err)))
+
+
+def setup_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model')
+    parser.add_argument('src')
+    parser.add_argument('target')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    args = setup_args()
+    logging.info(args)
+    get_error(args.model, args.src, args.target)
+
+if __name__ == '__main__':
+    main()
