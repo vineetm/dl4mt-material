5244610325d9e13c742e5f4a2b0a2ec41ef20999
Vineet Kumar <vineeku6@in.ibm.com>
2016-03-29 17:08:26 +0530

    make training parameters available as command line args

--- session2/train.py
+++ session2/train.py
@@ -1,47 +1,64 @@
 import numpy
 import os
-import logging
+import logging, argparse
 from nmt import train
 
-def main(job_id, params):
+
+def setup_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('basedir', help='base directory')
+    parser.add_argument('wordvec', help='word2vec trained file')
+    parser.add_argument('model', help='model name')
+    parser.add_argument('--srcwords', default=60000, type=int, help='src vocabulary size')
+    parser.add_argument('--targetwords', default=30000, type=int, help='target vocabulary size')
+    parser.add_argument('--dimword', default=300, type=int, help='word embedding dimension')
+    parser.add_argument('--dimhidden', default=300, type=int, help='hidden layer dimension')
+    parser.add_argument('--batch', default=128, type=int, help='Batch size')
+    parser.add_argument('--dispfreq', default=500, type=int, help='Display Frequency')
+    parser.add_argument('--savefreq', default=500, type=int, help='Save Frequency')
+    parser.add_argument('--validfreq', default=500, type=int, help='Validation Frequency')
+    parser.add_argument('--samplefreq', default=500, type=int, help='Sample Frequency')
+    parser.add_argument('--maxlen', default=50, type=int, help='Maximum length of sequence')
+    parser.add_argument('--lr', default=0.0001, type=float, help='Learning Rate')
+    parser.add_argument('--clipc', default=1.0, type=float, help='Gradient Clip threshold')
+    parser.add_argument('--decay', default=0.0, type=float, help='Decay rate')
+    args = parser.parse_args()
+    return args
+
+
+def main():
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-    validerr = train(saveto=params['model'][0],
-                     reload_=params['reload'][0],
-                     dim_word=params['dim_word'][0],
-                     dim=params['dim'][0],
-                     n_words=params['n-words'][0],
-                     n_words_src=params['n-words-src'][0],
-                     decay_c=params['decay-c'][0],
-                     clip_c=params['clip-c'][0],
-                     lrate=params['learning-rate'][0],
-                     optimizer=params['optimizer'][0], 
+    args = setup_args()
+    logging.info(args)
+
+    validerr = train(saveto=args.model + '.npz',
+                     reload_=False,
+                     dim_word=args.dimhidden,
+                     dim=args.dimword,
+                     n_words=args.targetwords,
+                     n_words_src=args.srcwords,
+                     decay_c=args.decay,
+                     clip_c=args.clipc,
+                     lrate=args.lr,
+                     optimizer='adam',
                      patience=1000,
-                     maxlen=50,
-                     batch_size=128,
-                     valid_batch_size=128,
-                     validFreq=500,
-                     dispFreq=500,
-                     saveFreq=500,
-                     sampleFreq=500,
-		     baseDir='/u/vineeku6/storage/train-data/yans',
-		     word2vecFile='/u/vineeku6/storage/data/yahoo-answers/word2vec/yans.vectors',
+                     maxlen=args.maxlen,
+                     batch_size=args.batch,
+                     valid_batch_size=args.batch,
+                     validFreq=args.validfreq,
+                     dispFreq=args.dispfreq,
+                     saveFreq=args.savefreq,
+                     sampleFreq=args.samplefreq,
+                     baseDir=args.basedir,
+                     word2vecFile=args.wordvec,
                      datasets=['train_src.txt', 'train_target.txt'],
                      valid_datasets=['valid_src.txt', 'valid_target.txt'],
                      dictionaries=['src.txt.pkl', 'target.txt.pkl'],
-                     use_dropout=params['use-dropout'][0],
-		     overwrite=True)
+                     use_dropout=False,
+                     overwrite=True)
     return validerr
 
+
 if __name__ == '__main__':
-    main(0, {
-        'model': ['test.npz'],
-        'dim_word': [300],
-        'dim': [300],
-        'n-words': [30000], 
-        'n-words-src': [80000], 
-        'optimizer': ['adam'],
-        'decay-c': [0.], 
-        'clip-c': [1.], 
-        'use-dropout': [False],
-        'learning-rate': [0.0001],
-        'reload': [False]})
+    valid_error = main()
+    logging.info('FINAL Valid Error: '+ valid_error)
