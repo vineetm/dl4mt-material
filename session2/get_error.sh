ff70888517ea3784cca4bc199d4a5c9f43c06ed6
Vineet Kumar <vineeku6@in.ibm.com>
2016-03-29 16:42:41 +0530

    Update error model

--- session2/get_error.sh
+++ session2/get_error.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MODEL=$1
 SUFFIX=$2
-SRC='/u/vineeku6/data/question-generation/yahoo-answers/valid_a.txt'
-TGT='/u/vineeku6/data/question-generation/yahoo-answers/valid_q.txt'
+SRC='/u/vineeku6/storage/train-data/yans/valid_src.txt'
+TGT='/u/vineeku6/storage/train-data/yans/valid_target.txt'
 jbsub -name test-$SUFFIX -out test-$SUFFIX.out -cores 2+1 -mem 10g -queue x86_short python get_error.py $MODEL $SRC $TGT
