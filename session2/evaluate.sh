e126bdcf56188cabaf54bc5762c2acd54cbe5ab7
Vineet Kumar <vineeku6@in.ibm.com>
2016-05-03 13:48:32 +0530

    Update report for bigram BLEU score

--- session2/evaluate.sh
+++ session2/evaluate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 MODEL=$1
-INPUT='/u/vineeku6/storage/train-data/ellipsis/unk-500/valid_src.txt'
-GOLD='/u/vineeku6/storage/train-data/ellipsis/unk-500/valid_target.txt'
+INPUT='/u/vineeku6/storage/train-data/ellipsis/unk-200-v2/valid_src.txt'
+GOLD='/u/vineeku6/storage/train-data/ellipsis/unk-200-v2/valid_target.txt'
 OUT=out-$MODEL.txt
 
 python translate_all.py $MODEL.npz $INPUT $OUT
--- session2/report.py
+++ session2/report.py
@@ -29,7 +29,7 @@ def main():
     gold_lines = codecs.open(args.gold, 'r', 'utf-8').readlines()
     gold_lines_nounk = codecs.open(args.gold + '.nounk', 'r', 'utf-8').readlines()
 
-    data = ['Src', 'Src_UNK', 'Target_UNK', 'Target', 'Gold_UNK', 'Gold', 'BLEU']
+    data = ['Src', 'Src_UNK', 'Target_UNK', 'Target', 'Gold_UNK', 'Gold', 'BLEU', 'BLUE_Bigram']
     csv_f.writerow(data)
 
     num_lines = len(gold_lines)
@@ -37,6 +37,7 @@ def main():
 
 
     bleu_scores = []
+    bleu_scores_bigram = []
     for index in range(num_lines):
         data = []
         data.append(src_lines_nounk[index].strip())
@@ -48,16 +49,19 @@ def main():
         data.append(gold_lines[index].strip())
         data.append(gold_lines_nounk[index].strip())
 
-	if len(target_lines[index].split()) == 0:
+	if len(target_lines[index].split()) < 2:
 		bleu_score = 0.0
+		bleu_score_bigram = 0.0
 	else:
         	bleu_score = bleu([gold_lines[index].split()], target_lines[index].split(), weights=(1.0,))
+		bleu_score_bigram = bleu([gold_lines[index].split()], target_lines[index].split(), weights=(0.5, 0.5))
 	logging.info('sentence:%d bleu:%f'%(index, bleu_score))
         bleu_scores.append(bleu_score)
+        bleu_scores_bigram.append(bleu_score_bigram)
         data.append(str(bleu_score))
         csv_f.writerow(data)
 
-    logging.info('Average BLEU Score: %f'% np.mean(bleu_scores))
+    logging.info('Average BLEU Score: %f Bigram: %f'% (np.mean(bleu_scores), np.mean(bleu_scores_bigram)))
 
 if __name__ == '__main__':
     main()
