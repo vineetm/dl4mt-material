8dd8ccb79af85d93e6dc32d6d7e303a23986b40d
Vineet Kumar <vineeku6@in.ibm.com>
2016-05-04 14:43:20 +0530

    Add fourgram BLEU score

--- session2/report.py
+++ session2/report.py
@@ -29,7 +29,7 @@ def main():
     gold_lines = codecs.open(args.gold, 'r', 'utf-8').readlines()
     gold_lines_nounk = codecs.open(args.gold + '.nounk', 'r', 'utf-8').readlines()
 
-    data = ['Src', 'Src_UNK', 'Target_UNK', 'Target', 'Gold_UNK', 'Gold', 'BLEU', 'BLUE_Bigram']
+    data = ['Src', 'Src_UNK', 'Target_UNK', 'Target', 'Gold_UNK', 'Gold', 'BLEU', 'BLUE2', 'BLEU4']
     csv_f.writerow(data)
 
     num_lines = len(gold_lines)
@@ -38,6 +38,8 @@ def main():
 
     bleu_scores = []
     bleu_scores_bigram = []
+    bleu_scores_fourgram = []
+
     for index in range(num_lines):
         data = []
         data.append(src_lines_nounk[index].strip())
@@ -49,19 +51,26 @@ def main():
         data.append(gold_lines[index].strip())
         data.append(gold_lines_nounk[index].strip())
 
-	if len(target_lines[index].split()) < 2:
-		bleu_score = 0.0
-		bleu_score_bigram = 0.0
-	else:
-        	bleu_score = bleu([gold_lines[index].split()], target_lines[index].split(), weights=(1.0,))
-		bleu_score_bigram = bleu([gold_lines[index].split()], target_lines[index].split(), weights=(0.5, 0.5))
-	logging.info('sentence:%d bleu:%f'%(index, bleu_score))
+        if len(target_lines[index].split()) < 4:
+            bleu_score = 0.0
+            bleu_score_bigram = 0.0
+            bleu_score_fourgram = 0.0
+        else:
+            bleu_score = bleu([gold_lines[index].split()], target_lines[index].split(), weights=(1.0,))
+            bleu_score_bigram = bleu([gold_lines[index].split()], target_lines[index].split(), weights=(0.5, 0.5))
+            bleu_score_fourgram = bleu([gold_lines[index].split()], target_lines[index].split(), weights=(0.25, 0.25, 0.25, 0.25))
+
+        logging.info('sentence:%d bleu:%f'%(index, bleu_score))
         bleu_scores.append(bleu_score)
         bleu_scores_bigram.append(bleu_score_bigram)
+        bleu_scores_fourgram.append(bleu_score_fourgram)
         data.append(str(bleu_score))
+        data.append(str(bleu_score_bigram))
+        data.append(str(bleu_score_fourgram))
         csv_f.writerow(data)
 
-    logging.info('Average BLEU Score: %f Bigram: %f'% (np.mean(bleu_scores), np.mean(bleu_scores_bigram)))
+    logging.info('Average BLEU Score: %f Bigram: %f Fourgram: %f'% (np.mean(bleu_scores), np.mean(bleu_scores_bigram),
+                                                                    np.mean(bleu_scores_fourgram)))
 
 if __name__ == '__main__':
     main()
