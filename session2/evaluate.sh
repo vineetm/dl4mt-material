767a0ec0212712d05a536209fbd5a680ac3fee47
Vineet Kumar <vineeku6@in.ibm.com>
2016-05-05 13:45:34 +0530

    Add evaluation using moses bleu script

--- session2/evaluate.sh
+++ session2/evaluate.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 MODEL=$1
-INPUT='/u/vineeku6/storage/train-data/ellipsis/unk-200-v2/valid_src.txt'
-GOLD='/u/vineeku6/storage/train-data/ellipsis/unk-200-v2/valid_target.txt'
+INPUT='/u/vineeku6/storage/train-data/ellipsis/raw-5/valid_src.txt'
+GOLD='/u/vineeku6/storage/train-data/ellipsis/raw-5/valid_target.txt'
 OUT=out-$MODEL.txt
 
-python translate_all.py $MODEL.npz $INPUT $OUT
+python translate_all.py $MODEL.npz $INPUT $OUT --all
+#python translate_all.py $MODEL.npz $INPUT $OUT
+
 python convert_symbols.py $INPUT $OUT
+./multi-bleu.perl $GOLD.nounk < $OUT.nounk &> bleu-$MODEL.out
+
 python report.py $INPUT $OUT $GOLD $MODEL 
