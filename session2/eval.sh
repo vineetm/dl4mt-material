c3fee0151314fdcc4dc8c21ec4adb1b26575d7a1
Vineet Kumar vineeku6@in.ibm.com <vineeku6@dccxl002.pok.ibm.com>
2016-06-22 04:51:03 -0400

    Fix helper scripts

--- session2/eval.sh
+++ session2/eval.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-UNK_20='/u/vineeku6/data-el/fixed-v2/unk-seq4'
+UNK_20='/u/vineeku6/data-el/entity/freq-20'
 
 BASE=$UNK_20
 ./submit-eval.sh ellipsis-exp00 $BASE
@@ -61,5 +61,25 @@ BASE=$UNK_20
 ./submit-eval.sh ellipsis-exp28 $BASE
 
 ./submit-eval.sh ellipsis-exp29 $BASE
+
+./submit-eval.sh ellipsis-exp30 $BASE
+
+./submit-eval.sh ellipsis-exp31 $BASE
+
+./submit-eval.sh ellipsis-exp32 $BASE
+
+./submit-eval.sh ellipsis-exp33 $BASE
+
+./submit-eval.sh ellipsis-exp34 $BASE
+
+./submit-eval.sh ellipsis-exp35 $BASE
+
+./submit-eval.sh ellipsis-exp36 $BASE
+
+./submit-eval.sh ellipsis-exp37 $BASE
+
+./submit-eval.sh ellipsis-exp38 $BASE
+
+./submit-eval.sh ellipsis-exp39 $BASE
 
 
--- session2/jobs.sh
+++ session2/jobs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-UNK_20='/u/vineeku6/data-el/fixed-v2/unk-seq4'
+DATA='/u/vineeku6/data-el/entity/freq-20'
 WORD_VEC_300='/u/vineeku6/storage/word-embeddings/trained/wiki-300d-ep4/wiki.vectors'
 WORD_VEC_200='/u/vineeku6/storage/word-embeddings/trained/wiki-200d/wiki.vectors'
 WORD_VEC_100='/u/vineeku6/storage/word-embeddings/trained/wiki-100d/wiki.vectors'
@@ -8,8 +8,8 @@ WORD_VEC_50='/u/vineeku6/storage/word-embeddings/trained/wiki-50d/wiki.vectors'
 QUEUE='x86_short'
 #QUEUE='x86_excl'
 
-BASE=$UNK_20
-VOCAB=847
+BASE=$DATA
+VOCAB=742
 
 ##Experiments with d=50
 WORD_VEC=$WORD_VEC_50
@@ -99,6 +99,7 @@ MODEL='ellipsis-exp19'
 ALPHA=0.0001
 jbsub -name $MODEL -out $MODEL.out -mem 10g -queue $QUEUE -cores 2+1 python train.py $BASE $WORD_VEC $MODEL --srcwords $VOCAB --targetwords $VOCAB --dimhidden $H --dimword $D --alphac $ALPHA
 
+##Experiments with d=200
 WORD_VEC=$WORD_VEC_200
 D=200
 
@@ -139,5 +140,49 @@ ALPHA=0.001
 jbsub -name $MODEL -out $MODEL.out -mem 10g -queue $QUEUE -cores 2+1 python train.py $BASE $WORD_VEC $MODEL --srcwords $VOCAB --targetwords $VOCAB --dimhidden $H --dimword $D --alphac $ALPHA
 
 MODEL='ellipsis-exp29'
+ALPHA=0.0001
+jbsub -name $MODEL -out $MODEL.out -mem 10g -queue $QUEUE -cores 2+1 python train.py $BASE $WORD_VEC $MODEL --srcwords $VOCAB --targetwords $VOCAB --dimhidden $H --dimword $D --alphac $ALPHA
+
+##Experiments with d=300
+WORD_VEC=$WORD_VEC_200
+D=300
+
+H=300
+MODEL='ellipsis-exp30'
+jbsub -name $MODEL -out $MODEL.out -mem 10g -queue $QUEUE -cores 2+1 python train.py $BASE $WORD_VEC $MODEL --srcwords $VOCAB --targetwords $VOCAB --dimhidden $H --dimword $D
+
+MODEL='ellipsis-exp31'
+ALPHA=0.1
+jbsub -name $MODEL -out $MODEL.out -mem 10g -queue $QUEUE -cores 2+1 python train.py $BASE $WORD_VEC $MODEL --srcwords $VOCAB --targetwords $VOCAB --dimhidden $H --dimword $D --alphac $ALPHA
+
+MODEL='ellipsis-exp32'
+ALPHA=0.01
+jbsub -name $MODEL -out $MODEL.out -mem 10g -queue $QUEUE -cores 2+1 python train.py $BASE $WORD_VEC $MODEL --srcwords $VOCAB --targetwords $VOCAB --dimhidden $H --dimword $D --alphac $ALPHA
+
+MODEL='ellipsis-exp33'
+ALPHA=0.001
+jbsub -name $MODEL -out $MODEL.out -mem 10g -queue $QUEUE -cores 2+1 python train.py $BASE $WORD_VEC $MODEL --srcwords $VOCAB --targetwords $VOCAB --dimhidden $H --dimword $D --alphac $ALPHA
+
+MODEL='ellipsis-exp34'
+ALPHA=0.0001
+jbsub -name $MODEL -out $MODEL.out -mem 10g -queue $QUEUE -cores 2+1 python train.py $BASE $WORD_VEC $MODEL --srcwords $VOCAB --targetwords $VOCAB --dimhidden $H --dimword $D --alphac $ALPHA
+
+H=200
+MODEL='ellipsis-exp35'
+jbsub -name $MODEL -out $MODEL.out -mem 10g -queue $QUEUE -cores 2+1 python train.py $BASE $WORD_VEC $MODEL --srcwords $VOCAB --targetwords $VOCAB --dimhidden $H --dimword $D
+
+MODEL='ellipsis-exp36'
+ALPHA=0.1
+jbsub -name $MODEL -out $MODEL.out -mem 10g -queue $QUEUE -cores 2+1 python train.py $BASE $WORD_VEC $MODEL --srcwords $VOCAB --targetwords $VOCAB --dimhidden $H --dimword $D --alphac $ALPHA
+
+MODEL='ellipsis-exp37'
+ALPHA=0.01
+jbsub -name $MODEL -out $MODEL.out -mem 10g -queue $QUEUE -cores 2+1 python train.py $BASE $WORD_VEC $MODEL --srcwords $VOCAB --targetwords $VOCAB --dimhidden $H --dimword $D --alphac $ALPHA
+
+MODEL='ellipsis-exp38'
+ALPHA=0.001
+jbsub -name $MODEL -out $MODEL.out -mem 10g -queue $QUEUE -cores 2+1 python train.py $BASE $WORD_VEC $MODEL --srcwords $VOCAB --targetwords $VOCAB --dimhidden $H --dimword $D --alphac $ALPHA
+
+MODEL='ellipsis-exp39'
 ALPHA=0.0001
 jbsub -name $MODEL -out $MODEL.out -mem 10g -queue $QUEUE -cores 2+1 python train.py $BASE $WORD_VEC $MODEL --srcwords $VOCAB --targetwords $VOCAB --dimhidden $H --dimword $D --alphac $ALPHA
