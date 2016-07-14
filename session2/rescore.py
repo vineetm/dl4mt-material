5f92dc8ff015602bad343e16d33d46f7c8029cb5
Vineet Kumar <vineeku6@in.ibm.com>
2016-07-14 14:14:46 +0530

    Print features

--- session2/rescore.py
+++ session2/rescore.py
@@ -2,9 +2,15 @@ import argparse, logging, codecs
 from translation_model import TranslationModel
 from collections import OrderedDict
 import commands
+from nltk.corpus import stopwords
+
+stopw = set(stopwords.words('english'))
 
 SVM_RANK_DATA='.svm_rank.data'
 
+Q1 = 0
+A1 = 1
+Q2 = 2
 
 '''
 1) Translate Source lines as per model
@@ -71,8 +77,33 @@ def replace_symbols(line, unk_map):
     return ' '.join(new_tokens)
 
 
+def get_fraction_keywords_match(gold, hyp):
+    rem_kw = gold - hyp
+    return len(gold) - len(rem_kw)
+
+
+def generate_features(src_line, translation):
+    features = []
+    translation_kw = set(translation.split()) - stopw
+
+    parts = src_line.split('END')
+    q2_bleu = get_bleu_score(parts[Q2], translation)
+    features.append(q2_bleu)
+
+    q2_keywords = set(parts[Q2].split()) - stopw
+    features.append(get_fraction_keywords_match(q2_keywords, translation_kw))
+
+    return features
+
 def write_train_data(fw, orig_id, train_id, translations, scores, scores_index, src_line):
-    fw.write('Original ID: %d'% orig_id)
+    fw.write('# Original ID: %d Source:%s'% (orig_id, src_line))
+
+    for num, id in enumerate(scores_index):
+        features = generate_features(src_line, translations[id][1])
+        features_kv = ['%d:%d'%(index+1, feature) for index,feature in enumerate(features)]
+        rank = len(scores_index) - num
+        fw.write('#Translation: %s'% translations[id][1])
+        fw.write('%d qid:%d %s'%(rank, train_id, ' '.join(features_kv)))
 
 
 '''
@@ -88,7 +119,7 @@ def main():
     src_lines_nounk = codecs.open(args.source + args.suffix, 'r', 'utf-8').readlines()
     gold_lines = codecs.open(args.gold + args.suffix, 'r', 'utf-8').readlines()
 
-    fw = codecs.open(args.model + SVM_RANK_DATA + 'w', 'utf-8')
+    fw = codecs.open(args.model + SVM_RANK_DATA, 'w', 'utf-8')
 
     tm = TranslationModel(args.model)
     num_all_zeros = 0
